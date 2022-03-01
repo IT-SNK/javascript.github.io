cc5fa86a8c3fcc3d3a9a5d6afbf51f0475e715b1
IT-SNK <90689814+IT-SNK@users.noreply.github.com>
2022-03-01 20:07:39 +0300

    add js

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+let Tc = 7;
+let Tf = (9 / 5) * Tc + 32;
+alert(Tf);
+
+
+let name = 'Василий';
+let admin = name;
+alert (admin);
+
+let first = 1000;
+let second = '108';
+let result = first + second;
+alert(result);
